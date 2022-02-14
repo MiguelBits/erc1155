1ea7e3d87a280e3621371e9f21c426f3e8e596d9
Miguel de Vasconcelos <mcv.cooper@gmail.com>
2022-02-14 16:49:03 +0000

    get staked time left

--- nft_duel.sol
+++ nft_duel.sol
@@ -10,7 +10,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Galaxy_heroes is ERC1155{
-    
+    //Events
+    event hasStakedChanged(bool status, uint tokenId);
+
+
+
     //ENUM
     enum Hero_Class{ROBOT, GOD, SUPERHUMAN, ALIEN, ANIMAL, DARKLINK}
     enum Item_Class{POTION, HERB, MYSTIC, WEAPON, MINERIUM, NOVA}
@@ -32,7 +36,7 @@ contract Galaxy_heroes is ERC1155{
     address private admin;
 
     //NFT associated with a TokenId_            MAPPINGS
-    mapping(Hero_Class => Counters.Counter) heroClass_amountMinted;
+    mapping(uint => Counters.Counter) heroClass_amountMinted;
     mapping(uint => uint) tokenId_classId;
     mapping(uint256 => string) private _tokenURIs;
     mapping(uint256 => NFT) public heroTokenId_ToNFT;
@@ -84,6 +88,8 @@ contract Galaxy_heroes is ERC1155{
         heroTokenId_ToNFT[id].stars += 1;
         //lockup time period
         tokenId_stakeTimeEnd[id] = block.timestamp + 600;
+
+        emit hasStakedChanged(true,id);
     }
 
     function unstake(uint id) public isStaked(id) isHeroOwner(id){
@@ -91,6 +97,8 @@ contract Galaxy_heroes is ERC1155{
         require(block.timestamp > tokenId_stakeTimeEnd[id],"Need more time!");
         heroTokenId_ToNFT[id].staked = false;
         _mint(msg.sender, coin,100000000,"");
+
+        emit hasStakedChanged(false,id);
     }
     
     //CREATION
@@ -102,81 +110,42 @@ contract Galaxy_heroes is ERC1155{
 
     //MINTERS
     //HERO
-    // robot
-    function heroMint_Robot() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.ROBOT].current();
+    function heroMint(uint class) external{
+        require(class == 1 || class == 2 || class == 3 || class == 4 || class == 5 || class == 6);
 
+        //get counter
         uint tokenId = _heroTokenIds.current();
-  
+        //mint gen ID
         _mint(msg.sender, tokenId,1, "");
-        NFT memory new_hero = NFT(Hero_Class.ROBOT,1,100,false);
-        hero_collection.push(new_hero);
-        heroTokenId_ToNFT[tokenId] = new_hero;
-        Hero_AtAddress[msg.sender].push(tokenId);
-        heroTokenId_ToOwner[tokenId] = msg.sender;
         
-        tokenId_classId[tokenId] = class_id;
-
-        _heroTokenIds.increment();
-        heroClass_amountMinted[Hero_Class.ROBOT].increment();
-        
-    }
-    // god
-    function heroMint_God() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.GOD].current();
- 
-        uint tokenId = _heroTokenIds.current();
-
-        _mint(msg.sender, tokenId,1, "");
-
-        NFT memory new_hero = NFT(Hero_Class.GOD,1,100,false);
-        hero_collection.push(new_hero);
-        heroTokenId_ToNFT[tokenId] = new_hero;
-        Hero_AtAddress[msg.sender].push(tokenId);
-        heroTokenId_ToOwner[tokenId] = msg.sender;
-        _heroTokenIds.increment();
-        
-        tokenId_classId[tokenId] = class_id;
-
-        heroClass_amountMinted[Hero_Class.GOD].increment();
-
-        
-    }
-    // superhuman
-    function heroMint_Superhuman() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.SUPERHUMAN].current();
-        uint tokenId = _heroTokenIds.current();
-
-        _mint(msg.sender, tokenId,1, "");
-
-        NFT memory new_hero = NFT(Hero_Class.SUPERHUMAN,1,100,false);
-        hero_collection.push(new_hero);
-        heroTokenId_ToNFT[tokenId] = new_hero;
-        Hero_AtAddress[msg.sender].push(tokenId);
-        heroTokenId_ToOwner[tokenId] = msg.sender;
-        _heroTokenIds.increment();
-                
-        tokenId_classId[tokenId] = class_id;
-
-        heroClass_amountMinted[Hero_Class.SUPERHUMAN].increment();
-        
-    }
-    // alien
-    function heroMint_Alien() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.ALIEN].current();
-        uint tokenId = _heroTokenIds.current();
-
-        _mint(msg.sender, tokenId,1, "");
-
-        NFT memory new_hero = NFT(Hero_Class.ALIEN,1,100,false);
+        //new hero with class id logic
+        uint class_id;
+        NFT memory new_hero;
+        if(class == 1){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.ROBOT,1,100,false);
+        }
+        else if(class == 2){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.GOD,1,100,false);
+        }
+        else if(class == 3){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.SUPERHUMAN,1,100,false);
+        }
+        else if(class == 4){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.ALIEN,1,100,false);
+        }
+        else if(class == 5){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.ANIMAL,1,100,false);
+        }
+        else if(class == 6){
+            class_id = heroClass_amountMinted[class].current();
+            new_hero = NFT(Hero_Class.DARKLINK,1,100,false);
+        }
+        //update mappings
         hero_collection.push(new_hero);
         heroTokenId_ToNFT[tokenId] = new_hero;
         Hero_AtAddress[msg.sender].push(tokenId);
@@ -185,50 +154,7 @@ contract Galaxy_heroes is ERC1155{
         
         tokenId_classId[tokenId] = class_id;
 
-        heroClass_amountMinted[Hero_Class.ALIEN].increment();
-        
-    }
-    // animal
-    function heroMint_Animal() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.ANIMAL].current();
-        uint tokenId = _heroTokenIds.current();
-
-        _mint(msg.sender, tokenId,1, ""); 
-
-        NFT memory new_hero = NFT(Hero_Class.ANIMAL,1,100,false);
-        hero_collection.push(new_hero);
-        heroTokenId_ToNFT[tokenId] = new_hero;
-        Hero_AtAddress[msg.sender].push(tokenId);
-        heroTokenId_ToOwner[tokenId] = msg.sender;
-        _heroTokenIds.increment();
-                
-        tokenId_classId[tokenId] = class_id;
-
-        heroClass_amountMinted[Hero_Class.ANIMAL].increment();
-        
-    }
-    // darklink
-    function heroMint_Darklink() external {   
-        // e.g. the buyer wants 100 tokens, needs to send 500 wei
-        //require(coinBalance() > 100 * coinPrice, "Need to send exact amount of wei");  
-        uint class_id = heroClass_amountMinted[Hero_Class.DARKLINK].current();
-        uint tokenId = _heroTokenIds.current();
-
-        _mint(msg.sender, tokenId,1, "");
-
-        NFT memory new_hero = NFT(Hero_Class.DARKLINK,1,100,false);
-        hero_collection.push(new_hero);
-        heroTokenId_ToNFT[tokenId] = new_hero;
-        Hero_AtAddress[msg.sender].push(tokenId);
-        heroTokenId_ToOwner[tokenId] = msg.sender;
-        _heroTokenIds.increment();
-        
-        tokenId_classId[tokenId] = class_id;
-
-        heroClass_amountMinted[Hero_Class.DARKLINK].increment();
-        
+        heroClass_amountMinted[class].increment();
     }
     //ITEM
     // to use payable to buy more rare
@@ -269,6 +195,11 @@ contract Galaxy_heroes is ERC1155{
     }
 
     //GETS
+    //amounts available
+    function getPopulation(uint class) public view returns(uint256 pop){
+        require(class == 1 || class == 2 || class == 3 || class == 4 || class == 5 || class == 6);
+        return heroClass_amountMinted[class].current();
+    }
     //account specific
     //coin
     function coinBalance() public view returns(uint coins){
@@ -331,7 +262,12 @@ contract Galaxy_heroes is ERC1155{
     }
         //staked - lockup time period
     function getStakedTimedLeft(uint id) public view returns(uint timeLeft){
-        return tokenId_stakeTimeEnd[id] - block.timestamp;
+        if(tokenId_stakeTimeEnd[id] > block.timestamp){
+            return tokenId_stakeTimeEnd[id] - block.timestamp;
+        }
+        else{
+            return 0;
+        }
     }
         //names
     function getNFT_class(uint id) public view returns(Hero_Class class){
